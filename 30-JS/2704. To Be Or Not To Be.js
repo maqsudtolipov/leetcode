a7af052c8c0cb3c4b55d845ccb089d774be55d1b
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-12-22 20:11:16 +0500

    Update 2704. To Be Or Not To Be.js

---
+++ 30-JS/2704. To Be Or Not To Be.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+const expect = function(val) {
+	return {
+		toBe: (val2) => {
+			if (val === val2) return true;
+			else throw new Error('Not Equal')
+		},
+		notToBe: (val2) => {
+			if (val !== val2) return true;
+			else throw new Error('Equal')
+		},
+	}
+};
