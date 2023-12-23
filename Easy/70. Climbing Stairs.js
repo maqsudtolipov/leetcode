d1dfc45b5780e31323ba2ed8c4763a5158a5fc5d
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-12-23 15:36:36 +0500

    Update 70. Climbing Stairs.js

--- Easy/70. Climbing Stairs.js
+++ Easy/70. Climbing Stairs.js
@@ -4,11 +4,27 @@
  */
 //  Recursion
 const climbStairs = function(n) {
-	if (n === 1) return 1;
-	if (n === 2) return 2;
+
+	if (n <= 2) return n;
 
 	return climbStairs(n - 1) + climbStairs(n - 2)
 };
 
-const result = climbStairs(44);
+const result = climbStairs(15);
 console.log(result)
+
+// MEMO
+const climbStairs = function(n) {
+	const brain = {};
+
+	const climbWithMemo = (c) => {
+		if (c <= 2) return c;
+		if (brain[c]) return brain[c];
+
+		brain[c] = climbWithMemo(c - 1) + climbWithMemo(c - 2);
+
+		return brain[c]
+	}
+
+	return climbWithMemo(n)
+};
