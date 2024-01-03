5b40f6a3f8a8eaaabce0ff04fcfa5b07ca733f87
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2024-01-03 09:09:21 +0500

    Update 2125. Number of Laser Beams in a Bank.js

--- Medium/2125. Number of Laser Beams in a Bank.js
+++ Medium/2125. Number of Laser Beams in a Bank.js
@@ -23,3 +23,14 @@ const numberOfBeams = function (bank) {
 };
 
 console.log(numberOfBeams(["011001", "000000", "010100", "001000"]));
+
+const numberOfBeamsOP = (bank) =>
+  [0, ...bank.map((row) => row.split("").filter((e) => e === "1").length)]
+    .filter((e) => e !== 0)
+    .map(
+      (e, i, arr) =>
+        i !== arr.length - 1 && arr.slice(i, i + 2).reduce((a, c) => a * c),
+    )
+    .reduce((a, c) => a + c);
+
+console.log(numberOfBeamsOP(["011001", "000000", "010100", "001000"]));
